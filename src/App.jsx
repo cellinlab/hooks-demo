66711702d3e109b071c4d8d57712eec07a86da3a
cell <cellinlab@gmail.com>
2023-02-12 18:20:31 +0800

    demo useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React, { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // 函数内声明变量，可以使用 useState，其接收一个参数，参数为初始值
+  const [list, setList] = useState([1, 2, 3, 4]);
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {
+        list.map((item, index) => {
+          return <div key={index}>{item}</div>
+        })
+      }
     </div>
   )
 }
